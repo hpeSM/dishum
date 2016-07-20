b1957f1f97be3a4342969a46d3fe28d34f3d9547
nagamano <nagamano@NAGAMANO2.asiapacific.hpqcorp.net>
2016-07-20 12:51:22 +0530

    commit 1

---
+++ Git Project/src/test1_master.java
@@ -0,0 +1,10 @@
+
+public class test1_master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Master");
+		System.out.println("Test_Master");
+
+		}
+}
